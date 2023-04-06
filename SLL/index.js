00358406ff70d773597adf8100ffa08cd196cc8d
Tyler Thibault <tyler.thibault@protonmail.com>
2023-04-06 08:48:56 -0600

    Update for Thursday w1

--- SLL/index.js
+++ SLL/index.js
@@ -167,12 +167,33 @@ class SinglyLinkedList {
   // ************************* WEDNESDAY *************************
 
   /**
- * Removes the last node of this list.
- * - Time: O(?).
+   * Removes the last node of this list.
+   * - Time: O(?).
  * - Space: O(?).
  * @returns {any} The data from the node that was removed.
- */
-  removeBack() { }
+  */
+  removeBack() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    // Only 1 node.
+    if (this.head.next === null) {
+      return this.removeHead();
+    }
+
+    // More than 1 node.
+    let runner = this.head;
+
+    while (runner.next.next) {
+      runner = runner.next;
+    }
+
+    // after while loop finishes, runner is now at 2nd to last node
+    const removedData = runner.next.data;
+    runner.next = null; // remove it from list
+    return removedData;
+  }
 
   /**
    * Determines whether or not the given search value exists in this list.
@@ -180,8 +201,18 @@ class SinglyLinkedList {
    * - Space: O(?).
    * @param {any} val The data to search for in the nodes of this list.
    * @returns {boolean}
-   */
-  contains(val) { }
+  */
+  contains(val) {
+    let runner = this.head
+
+    while (runner) {
+      if (runner.data == val) {
+        return true
+      }
+      runner = runner.next
+    }
+    return false
+  }
 
   /**
    * Determines whether or not the given search value exists in this list.
@@ -191,8 +222,16 @@ class SinglyLinkedList {
    * @param {?ListNode} current The current node during the traversal of this list
    *    or null when the end of the list has been reached.
    * @returns {boolean}
-   */
-  containsRecursive(val, current = this.head) { }
+  */
+  containsRecursive(val, current = this.head) {
+    if (current === null) {
+      return false;
+    }
+    if (current.data === val) {
+      return true;
+    }
+    return this.containsRecursive(val, current.next);
+  }
 
   // EXTRA
   /**
@@ -204,8 +243,54 @@ class SinglyLinkedList {
    * @param {ListNode} maxNode Keeps track of the node that contains the current
    *    max integer as it's data.
    * @returns {?number} The max int or null if none.
+  */
+  recursiveMax(runner = this.head, maxNode = this.head) {
+    if (this.head === null) {
+      return null;
+    }
+
+    if (runner === null) {
+      return maxNode.data;
+    }
+
+    if (runner.data > maxNode.data) {
+      maxNode = runner;
+    }
+
+    return this.recursiveMax(runner.next, maxNode);
+  }
+  // ************************* THURSDAY *************************
+
+  /**
+ * Retrieves the data of the second to last node in this list.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @returns {any} The data of the second to last node or null if there is no
+ *    second to last node.
+ */
+  secondToLast() { }
+
+  /**
+   * Removes the node that has the matching given val as it's data.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} val The value to compare to the node's data to find the
+   *    node to be removed.
+   * @returns {boolean} Indicates if a node was removed or not.
    */
-  recursiveMax(runner = this.head, maxNode = this.head) { }
+  removeVal(val) { }
+
+  // EXTRA
+  /**
+   * Inserts a new node before a node that has the given value as its data.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} newVal The value to use for the new node that is being added.
+   * @param {any} targetVal The value to use to find the node that the newVal
+   *    should be inserted in front of.
+   * @returns {boolean} To indicate whether the node was pre-pended or not.
+   */
+  prepend(newVal, targetVal) { }
 
   // ************************* UTILS *************************
 
@@ -248,14 +333,17 @@ const emptyList = new SinglyLinkedList();
 
 // console.log(emptyList.isEmpty())
 
-const singleNodeList = new SinglyLinkedList().insertAtBackMany([7]);
-console.log(singleNodeList)
+// const singleNodeList = new SinglyLinkedList().insertAtBackMany([7]);
+// console.log(singleNodeList)
 // const biNodeList = new SinglyLinkedList().insertAtBackMany([1, 2]);
 // const firstThreeList = new SinglyLinkedList().insertAtBackMany([1, 2, 3]);
 // const secondThreeList = new SinglyLinkedList().insertAtBackMany([4, 5, 6]);
-// const unorderedList = new SinglyLinkedList().insertAtBackMany([
-//   -5, -10, 4, -3, 6, 1, -7, -2,
-// ]);
+const unorderedList = new SinglyLinkedList().insertAtBackMany([
+  -5, -10, 4, -3, 6, 1, -7, -2,
+]);
+// unorderedList.printList().removeBack().printList()
+
+console.log(unorderedList.contains(99))
 
 /* node 4 connects to node 1, back to head */
 // const perfectLoopList = new SinglyLinkedList().insertAtBackMany([1, 2, 3, 4]);
